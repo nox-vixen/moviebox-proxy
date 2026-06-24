eb2a6b753d005515dc4359b9d2ed67f20c11f1fa
nox-vixen <nox.vixen.0@gmail.com>
2026-06-24 14:21:45 +0530

    fix indentation

--- moviebox_client.py
+++ moviebox_client.py
@@ -67,9 +67,9 @@ async def moviebox_get(path):
             headers=headers
         )
 
-return {
-    "url": str(r.url),
-    "status": r.status_code,
-    "headers": dict(r.headers),
-    "text": r.text[:2000]
-}
+        return {
+            "url": str(r.url),
+            "status": r.status_code,
+            "headers": dict(r.headers),
+            "text": r.text[:2000]
+        }
